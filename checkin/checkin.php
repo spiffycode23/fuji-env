9668899e8f2120f23f0f0f61893ad840419231ab
tester <tester@dash1.dyn6.twc.com>
2017-03-11 21:35:24 -0500

    Cleaned up checkin.php

--- checkin/checkin.php
+++ checkin/checkin.php
@@ -2,34 +2,18 @@
 
 if(isset($_POST['host']))
 {
-$ip = '';
-$method = 'default';
-	if(isset( $_POST['ip']))
-	{
-		$ip = $_POST['ip'];
-	}
-	else
-	{
-		$ip = "0.0.0.0";
-	}
-	if(isset( $_POST['method']))
-	{
-		$method = $_POST['method'];
-	}
-	else
-	{
-		$method = "curl/cron";
-	}
-
+	$ip  = (isset($_POST['ip'])) ? $_POST['ip'] : '0.0.0.0'; 
+	$method = (isset($_POST['method'])) ? $_POST['method'] : 'default';
 	$hostname = $_POST['host'];
-	echo "Data received: " . $hostname;
-	$sql = "INSERT INTO checkin(checkin_host, checkin_method,checkin_ip) VALUES ('$hostname','$method','$ip')";
-echo $sql;
 
-	   $dbhost = 'localhost';
-	   $dbuser = 'root';
-	   $dbpass = 'password';
-           $dbname = 'homesys';
+	echo "Data received: " . $hostname;
+
+	$sql = "INSERT INTO checkin(checkin_host, checkin_method,checkin_ip) VALUES ('$hostname','$method','$ip')";
+
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = 'password';
+	$dbname = 'homesys';
   	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass); 
 	try {
 		$CHECKNAME = $dbh->prepare($sql);
@@ -40,21 +24,10 @@ echo $sql;
 	{
 	 	echo $exception->getMessage();
 	}
-
-   
-	   echo "Entered data successfully\n";
-   
-	}
-	else 
-	{
-	    echo "No data received";
-	    ?>
- 	    <form action="checkin.php" method="post">
-	    <p>Hostname:</p>
-	    <input type="text" name="host" />
-	    <input type="submit" value="Test" name="test" />
-	    </form>
-	<?php
-        }
+}
+else 
+{
+    echo "No data received";
+}
 ?>
 
